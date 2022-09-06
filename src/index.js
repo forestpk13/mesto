f612d40884bf599b44ec9ee6f4ddc71ad7536210
Anton Kostylev <forestpk13@gmail.com>
2022-09-06 20:54:01 +0700

    webpack added

---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log('Hello, World!');
+const numbers = [2, 3, 5];
+
+// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers); // 4, 6, 10
